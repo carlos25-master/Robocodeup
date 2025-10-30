556690b96caf9a25f14b0b333b094a3b669f07d1
AndreiaSilvaTI <andreia-c-silva@live.com>
2025-10-30 19:51:11 -0300

    Update Bumblebee.java

--- Bumblebee.java
+++ Bumblebee.java
@@ -11,7 +11,7 @@ import robocode.*;
 	{
 	/**
 	 * run: Bumblebee's default behavior
-		*/
+		 */
 		public void run() {
 			
 	      // Cores do robô
@@ -30,14 +30,15 @@ import robocode.*;
 			setTurnRadarRight(360);
 			execute();
 						
-		// Movimentos do Robô
+		// Movimentos principais do Robô
 			ahead(100);
 			turnGunRight(360);
 			back(100);
 			turnGunRight(360);
-
+			}
+		
 		// Quando detectar um robô inimigo
-			public void onScannedRobot(ScannedRobotEvent
+			public void onScannedRobot(ScannedRobotEvent e) {
 														   
 	    // Calcular a distância ao inimigo
 			double distance = e.getDistance();
@@ -53,28 +54,28 @@ import robocode.*;
 		  if (getGunHeat() == 0 && Math.abs(getGunTurnRemaining()) < 10) {
 		  double firePower = Math.min(400 / distance, 3);
 		  setFire(firePower);
-		  }
+		  	}
 
 		// Movimento lateral para dificultar ser atingido
 			setTurnRight(e.getBearing() + 90 - 15);
 			setAhead(100);
-		  }
+		  	}
 
 		// Movimento lateral para dificultar ser atingido
 			setTurnRight(e.getBearing() + 90 - 15);
 			setAhead(100);
-		 }
+		 	}
 		
 		// Quando acertar outro robô com um tiro
 			public void onBulletHit(BulletHitEvent e) {
 			setAhead(50);
-				}
+			}
 			
 		// Quando for atingido por um tiro
 			public void onHitByBullet(HitByBulletEvent e) {
 			back(50);
 			turnRight(30);
-				}
+			}
 		
 		// Quando bater na parede
 			public void onHitWall(HitWallEvent e) {
@@ -91,4 +92,5 @@ import robocode.*;
 			}
 		}
 	}	
+
 
